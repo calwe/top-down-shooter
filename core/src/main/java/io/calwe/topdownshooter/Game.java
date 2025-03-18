5abf2837fd09542062bb278b389f2015dfc1c883
Callum Webb <callum.webb@stfc.ac.uk>
2025-03-18 09:12:40 +0000

    Seperate main file for launching game

---
+++ core/src/main/java/io/calwe/topdownshooter/Game.java
@@ -0,0 +1,12 @@
+package io.calwe.topdownshooter;
+
+import io.calwe.topdownshooter.screens.Play;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Game extends com.badlogic.gdx.Game {
+    @Override
+    public void create() {
+        // set the initial "screen" to the play screen (as we don't have a main menu)
+        setScreen(new Play());
+    }
+}
